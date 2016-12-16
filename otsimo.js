da8ee725e1eb61230ef93a6a511f7cce70273c3e
btk <iletisim@buraktokak.com>
2016-12-16 16:12:11 +0300

    Make changes for testing app.

--- otsimo.js
+++ otsimo.js
@@ -1,4 +1,4 @@
-var otsimo = function () {
+var otsimo = function() {
     "use strict";
     if (typeof otsimo !== "undefined") {
         return otsimo
@@ -18,13 +18,19 @@ var otsimo = function () {
         sound: true
     }
 
-    Object.defineProperty(otemp, "isWKWebView", { value: !!(window.webkit && window.webkit.messageHandlers) });
-    Object.defineProperty(otemp, "isUIWebView", { value: (typeof otsimonative !== 'undefined') });
-    Object.defineProperty(otemp, "iOS", { value: (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false) });
+    Object.defineProperty(otemp, "isWKWebView", {
+        value: !!(window.webkit && window.webkit.messageHandlers)
+    });
+    Object.defineProperty(otemp, "isUIWebView", {
+        value: (typeof otsimonative !== 'undefined')
+    });
+    Object.defineProperty(otemp, "iOS", {
+        value: (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false)
+    });
 
-    var getJSON = function (url, res) {
+    var getJSON = function(url, res) {
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
+        xmlhttp.onreadystatechange = function() {
             var status;
             var data;
             if (xmlhttp.readyState == 4) {
@@ -41,11 +47,11 @@ var otsimo = function () {
         xmlhttp.send();
     }
 
-    var __registerLoadingCallback = function (fn) {
+    var __registerLoadingCallback = function(fn) {
         __callbackStack.push(fn)
     }
 
-    var __callLoadingCallbacks = function () {
+    var __callLoadingCallbacks = function() {
         __isloaded = true
         for (var i = 0; i < __callbackStack.length; i++) {
             __callbackStack[i]()
@@ -53,7 +59,7 @@ var otsimo = function () {
         __callbackStack.splice(0, __callbackStack.length)
     }
 
-    otemp.log = function () {
+    otemp.log = function() {
         if (otemp.isWKWebView) {
             console.log.apply(console, arguments)
             window.webkit.messageHandlers.console.postMessage(JSON.stringify(arguments));
@@ -62,27 +68,37 @@ var otsimo = function () {
         }
     }
 
-    otemp.customevent = function (eventName, data) {
+    otemp.customevent = function(eventName, data) {
         if (otemp.isWKWebView) {
-            window.webkit.messageHandlers.analytics.postMessage({ event: eventName, data: data });
+            window.webkit.messageHandlers.analytics.postMessage({
+                event: eventName,
+                data: data
+            });
         } else if (otemp.isUIWebView) {
-            otsimonative.customevent({ event: eventName, data: data })
+            otsimonative.customevent({
+                event: eventName,
+                data: data
+            })
         } else {
             otemp.log("customevent", eventName, data)
         }
     }
 
-    otemp.quitgame = function () {
+    otemp.quitgame = function() {
         if (otemp.isWKWebView) {
-            window.webkit.messageHandlers.player.postMessage({ event: "quitgame" });
+            window.webkit.messageHandlers.player.postMessage({
+                event: "quitgame"
+            });
         } else if (otemp.isUIWebView) {
-            otsimonative.player({ event: "quitgame" })
+            otsimonative.player({
+                event: "quitgame"
+            })
         } else {
             otemp.log("quit game called")
         }
     }
 
-    otemp.run = function (fn) {
+    otemp.run = function(fn) {
         otemp.log("register function to run")
 
         if (__isloaded) {
@@ -94,15 +110,15 @@ var otsimo = function () {
         }
     }
 
-    otemp.onSettingsChanged = function (fn) {
+    otemp.onSettingsChanged = function(fn) {
         __settingsCallbacks.push(fn)
     }
 
-    otemp.onResolutionChanged = function(fn){
+    otemp.onResolutionChanged = function(fn) {
         __resolutionCallbacks.push(fn)
     }
 
-    otemp.__callSettingsCallbacks = function (settings, sound) {
+    otemp.__callSettingsCallbacks = function(settings, sound) {
         if (settings) {
             otemp.settings = settings
         }
@@ -113,24 +129,24 @@ var otsimo = function () {
         }
     }
 
-    otemp.__callResolutionCallbacks = function (width, height, orientation) {
+    otemp.__callResolutionCallbacks = function(width, height, orientation) {
         for (var i = 0; i < __resolutionCallbacks.length; i++) {
             __resolutionCallbacks[i](width, height, orientation)
         }
     }
 
-    otemp.init = function (options) {
+    otemp.init = function(options) {
         if (!options) {
             options = {}
         }
         otemp.log("initialize of bundle otsimo.js")
 
-        if (otemp.isWKWebView) {
+        if (otemp.isWKWebView && options.isTestApp) {
             otemp.log("sandbox won't be initializing")
             return
         }
 
-        if (otemp.isUIWebView) {
+        if (otemp.isUIWebView && options.isTestApp) {
             otemp.log("sandbox won't be initializing")
             return
         }
@@ -141,9 +157,13 @@ var otsimo = function () {
         otemp.width = options.width || 1024
         otemp.height = options.height || 768
         if (options.capabilities && Array === options.capabilities.constructor) {
-            Object.defineProperty(otemp, "capabilities", { value: options.capabilities });
+            Object.defineProperty(otemp, "capabilities", {
+                value: options.capabilities
+            });
         } else {
-            Object.defineProperty(otemp, "capabilities", { value: ["sandbox"] });
+            Object.defineProperty(otemp, "capabilities", {
+                value: ["sandbox"]
+            });
         }
         if (typeof options.debug != "undefined" && options.debug == false) {
             otemp.debug = false
@@ -153,7 +173,7 @@ var otsimo = function () {
         getJSON("otsimo.json", otemp.__initManifest)
     }
 
-    otemp.getLanguages = function () {
+    otemp.getLanguages = function() {
         var found = []
         if (typeof navigator !== 'undefined') {
             if (navigator.languages) { // chrome only; not an array, so can't use .push.apply instead of iterating
@@ -171,22 +191,30 @@ var otsimo = function () {
         return found
     }
 
-    otemp.saveLocalSettings = function (data) {
+    otemp.saveLocalSettings = function(data) {
         var sdata = JSON.stringify(data)
         if (otemp.isWKWebView) {
-            window.webkit.messageHandlers.save.postMessage({ event: "save", data: sdata });
+            window.webkit.messageHandlers.save.postMessage({
+                event: "save",
+                data: sdata
+            });
         } else if (otemp.isUIWebView) {
-            otsimonative.saveLocalSettings({ data: sdata })
+            otsimonative.saveLocalSettings({
+                data: sdata
+            })
         } else {
             otemp.log("saveLocalSettings", data)
             localStorage.setItem(otemp.__storageKey(), sdata);
         }
     }
 
-    otemp.loadLocalSettings = function (callback) {
+    otemp.loadLocalSettings = function(callback) {
         if (otemp.isWKWebView) {
             var id = otemp.__addSaveCallback(callback)
-            window.webkit.messageHandlers.save.postMessage({ event: "load", id: id });
+            window.webkit.messageHandlers.save.postMessage({
+                event: "load",
+                id: id
+            });
         } else if (otemp.isUIWebView) {
             otsimonative.loadLocalSettings(callback)
         } else {
@@ -211,17 +239,20 @@ var otsimo = function () {
         return text;
     }
 
-    otemp.__addSaveCallback = function (callback) {
+    otemp.__addSaveCallback = function(callback) {
         var id = makeid(4)
-        __saveCallbacks.push({ id: id, callback: callback })
+        __saveCallbacks.push({
+            id: id,
+            callback: callback
+        })
         return id
     }
 
-    otemp.__storageKey = function () {
+    otemp.__storageKey = function() {
         return otemp.manifest.unique_name
     }
 
-    otemp.__callSaveCallback = function (err, data, id) {
+    otemp.__callSaveCallback = function(err, data, id) {
         for (var i = 0; i < __saveCallbacks.length; i++) {
             var cc = ___saveCallbacks[i]
             if (cc.id == id) {
@@ -241,7 +272,7 @@ var otsimo = function () {
         }
     }
 
-    otemp.__init = function (options) {
+    otemp.__init = function(options) {
         otemp.log("__init called", options)
         otemp.settings = options.settings
         otemp.child = options.child
@@ -250,17 +281,21 @@ var otsimo = function () {
         otemp.sound = options.sound
         otemp.root = options.root
         if (options.capabilities && Array === options.capabilities.constructor) {
-            Object.defineProperty(otemp, "capabilities", { value: options.capabilities });
+            Object.defineProperty(otemp, "capabilities", {
+                value: options.capabilities
+            });
         } else {
-            Object.defineProperty(otemp, "capabilities", { value: ["sandbox"] });
+            Object.defineProperty(otemp, "capabilities", {
+                value: ["sandbox"]
+            });
         }
-        getJSON(otemp.root + "otsimo.json", function (err, manifest) {
+        getJSON(otemp.root + "otsimo.json", function(err, manifest) {
             if (err) {
                 otemp.log("Failed to get otsimo.json, status=", err)
             } else {
                 otemp.manifest = manifest
                 var langFile = otemp.root + manifest.kv_path + "/" + otemp.child.language + ".json"
-                getJSON(langFile, function (err, data) {
+                getJSON(langFile, function(err, data) {
                     if (err) {
                         otemp.log("failed to get kv, status", err)
                     } else {
@@ -274,7 +309,7 @@ var otsimo = function () {
         return true
     }
 
-    otemp.__initManifest = function (err, data) {
+    otemp.__initManifest = function(err, data) {
         if (err) {
             otemp.log("Failed to get otsimo.json, status=", err)
         } else {
@@ -283,7 +318,7 @@ var otsimo = function () {
         }
     }
 
-    otemp.__loadKeyValueStore = function () {
+    otemp.__loadKeyValueStore = function() {
         var sy = otemp.getLanguages()
         var lang = otemp.child.language || otemp.manifest.default_language;
         var langFile = otemp.manifest.kv_path + "/" + lang + ".json"
@@ -297,7 +332,7 @@ var otsimo = function () {
                 }
             }
         }
-        getJSON(langFile, function (err, data) {
+        getJSON(langFile, function(err, data) {
             if (err) {
                 otemp.log("failed to get kv, status", err)
             } else {
@@ -308,7 +343,7 @@ var otsimo = function () {
         })
     }
 
-    otemp.__initSettings = function (err, data) {
+    otemp.__initSettings = function(err, data) {
         if (err) {
             otemp.log("failed to get settings,status", err)
         } else {
@@ -321,33 +356,42 @@ var otsimo = function () {
             otemp.__loadKeyValueStore()
         }
     }
-    var tts = { __driver: null }
-    tts.speak = function (text) {
+    var tts = {
+        __driver: null
+    }
+    tts.speak = function(text) {
         if (tts.__driver) {
             return tts.__driver.speak(text);
         }
         return new Error("TTS Driver is not set")
     }
-    tts.setVoice = function (voice) {
+    tts.setVoice = function(voice) {
         if (tts.__driver) {
             return tts.__driver.setVoice(voice);
         }
         return new Error("TTS Driver is not set")
     }
-    tts.getVoice = function () {
+    tts.getVoice = function() {
         if (tts.__driver) {
             return tts.__driver.getVoice();
         }
         return new Error("TTS Driver is not set")
     }
-    tts.voiceList = function () {
+    tts.voiceList = function() {
         if (tts.__driver) {
             return tts.__driver.voiceList();
         }
         return new Error("TTS Driver is not set")
     }
-    tts.setDriver = function (driver) { tts.__driver = driver; }
-    tts.getDriver = function () { return tts.__driver; }
-    Object.defineProperty(otemp, "tts", { value: tts, writable: false });
+    tts.setDriver = function(driver) {
+        tts.__driver = driver;
+    }
+    tts.getDriver = function() {
+        return tts.__driver;
+    }
+    Object.defineProperty(otemp, "tts", {
+        value: tts,
+        writable: false
+    });
     return otemp
-} ()
+}()
